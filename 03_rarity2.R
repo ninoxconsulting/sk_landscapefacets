658af48fc3247066ed9b6023925d07595dff8f1a
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2024-04-12 16:17:51 -0700

    run through with the classifications for bedrock

--- 03_rarity2.R
+++ 03_rarity2.R
@@ -6,44 +6,85 @@ library(ggplot2)
 library(dplyr)
 
 ## actual size 
-srast = terra::rast(file.path("inputs", "sk_lf_3005.tif"))
-srastrc = terra::rast(file.path("outputs", "sk_lf_rockclass.tif"))
-srastrcd = terra::rast(file.path("outputs", "sk_lf_rockclassdet.tif"))
+#srast = terra::rast(file.path("inputs", "sk_lf_3005.tif"))
+
+#srast = srastrc = terra::rast(file.path("outputs", "sk_lf_rockclass.tif"))
+srast = terra::rast(file.path("outputs", "sk_lf_rockclassdet.tif"))
 
 
-
+srast
 
 # read in the rare csv file 
-rare <- read.csv("landscape_facets_summary.csv") %>%
-  dplyr::select(skeena_lfacet_3005,count, rare_under_10 )
+# rare <- read.csv(file.path("inputs","landscape_facets_summary.csv")) %>%
+#  dplyr::select(skeena_lfacet_3005,count, rare_under_10 )
 
+# read in the rare csv file 
+#rare <- read.csv(file.path("outputs","landscape_facet_summary_rc.csv")) %>%
+#  dplyr::select(layer1,count, rare_under_10 )
+
+# read in the rare csv file 
+rare <- read.csv(file.path("outputs","landscape_facet_summary_rcd.csv")) %>%
+  dplyr::select(layer1,count, rare_under_10 )
 
 #cutoff is count of 46127
 
 rare10 <- rare %>%
   filter(rare_under_10 == "y")
   
-#hist(rare$count)
-#hist(rare10$count)
-#quantile(rare10$count)
+hist(rare$count)
+hist(rare10$count)
+quantile(rare10$count)
  
 #quantile(rare10$count)
 #0%      25%      50%      75%     100% 
 #1.00   326.00  2807.00 12374.75 46127.00 
 
+# for rc (n = 5 option) 
+#0%  25%  50%  75% 100% 
+#1   28  170  438  953 
+
+
+# for rcdet (n = 5 option) 
+#0%   25%   50%   75%  100% 
+#1.0  12.5  62.0 132.5 268.0 
+ 
+
+
 
 
 # generate rare classes
+# 
+# rareq <- rare %>% 
+#  dplyr::mutate(rare_id = case_when(
+#     count < 326 ~ 1,
+#     count > 326 & count < 2807 ~ 2,
+#     count > 2807 & count < 12374 ~ 3,
+#     count > 12374 & count < 46127~ 4,
+#     .default = as.numeric(99)
+#   ))
+  
+# # rock class (n = 5) 
+# rareq <- rare %>% 
+#   dplyr::mutate(rare_id = case_when(
+#     count < 28 ~ 1,
+#     count > 28 & count < 170 ~ 2,
+#     count > 170 & count < 438 ~ 3,
+#     count > 438 & count < 953 ~ 4,
+#     .default = as.numeric(99)
+#   ))
 
+# rock class (n = 19) 
 rareq <- rare %>% 
- dplyr::mutate(rare_id = case_when(
-    count < 326 ~ 1,
-    count > 326 & count < 2807 ~ 2,
-    count > 2807 & count < 12374 ~ 3,
-    count > 12374 & count < 46127~ 4,
+  dplyr::mutate(rare_id = case_when(
+    count < 12 ~ 1,
+    count > 12 & count < 62 ~ 2,
+    count > 62 & count < 132 ~ 3,
+    count > 132 & count < 268 ~ 4,
     .default = as.numeric(99)
   ))
-  
+
+
+
 
 # # try reclass? Not working 
 # 
@@ -66,12 +107,19 @@ rareq <- rare %>%
 # classify the values into three groups 
 # all values >= 0 and <= 0.25 become 1, etc.
 
+# 
+# class1 <-rareq  %>% filter(rare_id == 1) %>% pull(skeena_lfacet_3005)
+# class2 <-rareq  %>% filter(rare_id == 2) %>% pull(skeena_lfacet_3005)
+# class3 <-rareq  %>% filter(rare_id == 3) %>% pull(skeena_lfacet_3005)
+# class4 <-rareq  %>% filter(rare_id == 4) %>% pull(skeena_lfacet_3005)
 
-class1 <-rareq  %>% filter(rare_id == 1) %>% pull(skeena_lfacet_3005)
-class2 <-rareq  %>% filter(rare_id == 2) %>% pull(skeena_lfacet_3005)
-class3 <-rareq  %>% filter(rare_id == 3) %>% pull(skeena_lfacet_3005)
-class4 <-rareq  %>% filter(rare_id == 4) %>% pull(skeena_lfacet_3005)
 
+# rock class
+
+class1 <-rareq  %>% filter(rare_id == 1) %>% pull(layer1)
+class2 <-rareq  %>% filter(rare_id == 2) %>% pull(layer1)
+class3 <-rareq  %>% filter(rare_id == 3) %>% pull(layer1)
+class4 <-rareq  %>% filter(rare_id == 4) %>% pull(layer1)
 
 rr <- srast
 
@@ -166,5 +214,8 @@ rclmat <- matrix(m, ncol=3, byrow=TRUE)
 rc1 <- classify(rr, rclmat, include.lowest=TRUE)
 
 
-terra::writeRaster(rc1, "sk_rarity_class.tif")
+#terra::writeRaster(rc1, "sk_rarity_class.tif")
 
+#terra::writeRaster(rc1,file.path("outputs", "sk_rarity_class.tif"))
+#terra::writeRaster(rc1,file.path("outputs", "sk_rarity_class_rc.tif"))
+terra::writeRaster(rc1,file.path("outputs", "sk_rarity_class_rcd.tif"))
