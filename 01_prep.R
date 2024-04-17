6376f6980e9e23616ad367156619a9776fb5a22d
gcperk <genevieve.c.perkins@gmail.com>
2024-04-17 15:44:10 -0700

    update prep script with sediment

--- 01_prep.R
+++ 01_prep.R
@@ -19,14 +19,21 @@ skeena = rast(file.path("inputs", "skeena_lfacet_3005.tif"))
 
 #skeena = rast("skeena_raw_lfacet.tif")
 
-# or use the base data on google drive 
 
-basedata = "C:\\Users\\genev\\OneDrive\\Documents\\02.Contracts\\00_data\\base_vector\\regions"
-basedata_soil  = "C:\\Users\\genev\\OneDrive\\Documents\\02.Contracts\\00_data\\base_vector\\bc"
+#basedata = "C:\\Users\\genev\\OneDrive\\Documents\\02.Contracts\\00_data\\base_vector\\regions"
+#basedata_soil  = "C:\\Users\\genev\\OneDrive\\Documents\\02.Contracts\\00_data\\base_vector\\bc"
 
 # keep as vect to use terra
-aoi <- vect(file.path(basedata, "SkeenaRegionBndry.shp"))
+#aoi <- vect(file.path(basedata, "SkeenaRegionBndry.shp"))
+#aoi_sf <- st_as_sf(aoi)
+
+#basedata = "C:\\Users\\genev\\OneDrive\\Documents\\02.Contracts\\00_data\\base_vector\\regions"
+basedata = "inputs"
+
+aoi <- vect(file.path(basedata, "SkeenaRegionBndry.gpkg"))
 aoi_sf <- st_as_sf(aoi)
+
+
 
 
 #PROJCS["NA_Lambert_Azimuthal_Equal_Area",GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433],METADATA["World",-180.0,-90.0,180.0,90.0,0.0,0.0174532925199433,0.0,1262]],PROJECTION["Lambert_Azimuthal_Equal_Area"],PARAMETER["False_Easting",0.0],PARAMETER["False_Northing",0.0],PARAMETER["Central_Meridian",-100.0],PARAMETER["Latitude_Of_Origin",45.0],UNIT["Meter",1.0]]
@@ -46,10 +53,29 @@ rocks <- bcdc_query_geodata("ef8476ed-b02d-4f5c-b778-0d44c9126144") |>
   select(ROCK_TYPE_DESCRIPTION, ROCK_TYPE_CODE, ROCK_CLASS) |>
   collect()
 
-skrocks <- rename_with(rocks, tolower)
+skrocks <- rename_with(rocks, tolower) %>% 
+  st_intersection(aoi_sf)%>% 
+  select(rock_type_description)
   
 
-st_write(skrocks, file.path("inputs", "skeena_soils.gpkg"))
+st_write(skrocks, file.path("inputs", "skeena_clip_soils.gpkg"))
+
+# merge the soild with sediment layer 
+
+sed <- st_read(file.path("inputs", "quat_sed.gpkg")) %>%
+  mutate(rock_type_description = "quaternary sediment") %>% 
+  select(rock_type_description)
+
+st_write(sed, file.path("inputs", "quad_sed_simple.gpkg"))
+
+
+
+# in QGIS use symetrical difference to remove sediment areas within the bedrock layer & UNION   WITH BEDROCK 
+
+br <- st_read(file.path("inputs", "sk_bedrock_sediment_raw.gpkg")) %>% 
+  select(rock_type_description)
+
+skrocks <- br
 
 # read in Paulas key and make a unique id values
 bedrock_key <- read_excel(file.path("inputs","Skeena_Geology for land facets April 2_2024.xlsx"), 
@@ -65,8 +91,8 @@ rock_class_class <- unique(bedrock_key$rock_class) #5
 rock_class_det_class <- unique(bedrock_key$rock_class_det) #19 
 
 
-rock_class_id <- bedrock_key %>% 
-  select(rock_class) %>% 
+rock_class_id <- bedrock_key %>%
+  select(rock_class) %>%
   distinct() %>%
   mutate(rock_class_no = seq(1,length(unique(rock_class)),1))
 
@@ -74,7 +100,6 @@ rock_class_det_id <-  bedrock_key %>%
   select(rock_class_det) %>%
   distinct() %>%
   mutate(rock_class_det_no = seq(1,length(unique(rock_class_det)),1))
-
 
 bedrock_key <- left_join(bedrock_key, rock_class_id ) %>% 
   left_join(rock_class_det_id) %>%
@@ -98,24 +123,24 @@ bedrock_key <- left_join(bedrock_key, rock_class_id ) %>%
 # 
 
 # combine the raster and key 
-skrocks <- skrocks %>% 
-  select(-rock_class)
+#skrocks <- skrocks #%>% 
+ # select(-rock_class)
 
 skrocks_out <- left_join(skrocks, bedrock_key, by = "rock_type_description") %>% 
-  select(-id, -bedrock_unit_id, -stratigraphic_age_code, -objectid, -rock_type_code )
+  select(-rock_class, -rock_class_no)
+ # select(-id, -bedrock_unit_id, -stratigraphic_age_code, -objectid, -rock_type_code )
 
 # convert to raster 
 #rast(skrocks_out)
 
 srockvec = as(skrocks_out,"SpatVector")
-rock_no = rasterize(srockvec, skeena, field="rock_class_no")
+#rock_no = rasterize(srockvec, skeena, field="rock_class_no")
 rockdet_no = rasterize(srockvec, skeena, field="rock_class_det_no")
 
-
-rock_no <- mask(rock_no, aoi)
+#rock_no <- mask(rock_no, aoi)
 rockdet_no<- mask(rockdet_no, aoi)
 
-writeRaster(rock_no, file.path("inputs", "sk_bedrock_class.tif"), overwrite = TRUE)
+#writeRaster(rock_no, file.path("inputs", "sk_bedrock_class.tif"), overwrite = TRUE)
 writeRaster(rockdet_no, file.path("inputs", "sk_bedrock_class_det.tif"), overwrite = TRUE)
 
 
@@ -129,7 +154,6 @@ srast = rast(file.path("inputs", "sk_lf_3005.tif"))
 # number of codes within Aoi 
 uval = length(unique(values(srast)))
 
-
 # remove the last two digits
 w <- app(srast, fun=function(x){ round(as.numeric(x) / 100, 0) } )
 
@@ -139,11 +163,10 @@ w
 
 
 # create new barcode for the new bedrock layers 
-
-sk_rockclass_cat <- w + rock_no
+#sk_rockclass_cat <- w + rock_no
 sk_rockclassdet_cat <- w + rockdet_no
 
-writeRaster(sk_rockclass_cat, file.path("outputs", "sk_lf_rockclass.tif"), overwrite = TRUE)
+#writeRaster(sk_rockclass_cat, file.path("outputs", "sk_lf_rockclass.tif"), overwrite = TRUE)
 writeRaster(sk_rockclassdet_cat, file.path("outputs", "sk_lf_rockclassdet.tif"), overwrite = TRUE)
 
 
@@ -151,10 +174,12 @@ writeRaster(sk_rockclassdet_cat, file.path("outputs", "sk_lf_rockclassdet.tif"),
 
 
 
+srast <- sk_rockclassdet_cat
+
 
 # Read in the skeena facets 
 
-unique(srast)
+#unique(srast)
 
 # number of codes within Aoi 
 uval = length(unique(values(srast)))
@@ -163,7 +188,7 @@ uval = length(unique(values(srast)))
 routdf <- as.data.frame(srast)
 
 # polt histogram
-ggplot2::ggplot(routdf, aes(skeena_lfacet_3005)) +
+ggplot2::ggplot(routdf, aes(lyr.1)) +
   ggplot2::geom_histogram(bins = uval) 
   #geom_freqpoly()
 
@@ -178,9 +203,9 @@ ggplot2::ggplot(routdf, aes(skeena_lfacet_3005)) +
 
 
 
-sumraster = function(inrast){
+#sumraster = function(inrast){
   
-  #inrast <- sk_rockclassdet_cat
+  inrast <- sk_rockclassdet_cat
   routdf <- as.data.frame(inrast)
   
   colnames(routdf) = "layer1"
@@ -190,28 +215,34 @@ sumraster = function(inrast){
     summarise(count = n())%>%
     mutate(total = sum(count))%>%
     rowwise() %>%
-    mutate(prop = (count/total)*100)%>%
+    mutate(pc = (count/total)*100)%>%
     arrange(count)
-  
 
     
   #mySum = t(apply(ids$prop, 1, cumsum))
-  ids <-within(ids, acc_sum <- cumsum(prop)*100)
-  ids <- ids %>% 
-    mutate(rare_under_10 = ifelse(acc_sum <10, "y", "n"))
-
-  return(ids)
+  ids <-within(ids, acc_sum <- cumsum(pc))
   
-}
+  ids <- ids %>% 
+    mutate(rarity_class = case_when(
+      acc_sum <= 1 ~ 5, 
+      acc_sum > 1 & acc_sum <=2 ~ 4,
+      acc_sum > 2 & acc_sum <4 ~ 3,
+      acc_sum > 4 & acc_sum <8 ~ 2,
+      acc_sum > 8 & acc_sum <16 ~ 1,
+      .default = as.numeric(999)
+    ))
+             
+             
+#return(ids)
+  
+#}
 
 
 # generate Summary tables 
+#ls_facet_sum_rcd <- sumraster(sk_rockclassdet_cat)
+#ls_facet_sum_rc <- sumraster(sk_rockclass_cat)
 
-ls_facet_sum_rcd <- sumraster(sk_rockclassdet_cat)
-
-ls_facet_sum_rc <- sumraster(sk_rockclass_cat)
-
-write_csv(ls_facet_sum_rcd, file.path("outputs", "landscape_facet_summary_rcd.csv"))
-write_csv(ls_facet_sum_rc, file.path("outputs", "landscape_facet_summary_rc.csv"))
+write_csv(  ids , file.path("outputs", "landscape_facet_summary_rcd.csv"))
+#write_csv(ls_facet_sum_rc, file.path("outputs", "landscape_facet_summary_rc.csv"))
 
          
