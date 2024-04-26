779473c9dd5c3ae7ce2b19359a9e91b31e7ff2c0
gcperk <genevieve.c.perkins@gmail.com>
2024-04-26 14:27:20 -0700

    fix undivided soil catergory and reclass

--- 01_prep.R
+++ 01_prep.R
@@ -1,5 +1,4 @@
-#ibrary(googlesheets4)
-#library(googledrive)
+
 library(terra)
 library(sf)
 library(ggplot2)
@@ -12,22 +11,15 @@ library(readxl)
 ## note all data in google drive in "inputs folder"
 
 
-
 # Read in the skeena facets 
-
-skeena = rast(file.path("inputs", "skeena_lfacet_3005.tif"))
-
-#skeena = rast("skeena_raw_lfacet.tif")
+skeena = rast(file.path("inputs", "sk_lf_3005.tif")) # use this one as clipped to skeeena region
 
 
 #basedata = "C:\\Users\\genev\\OneDrive\\Documents\\02.Contracts\\00_data\\base_vector\\regions"
 #basedata_soil  = "C:\\Users\\genev\\OneDrive\\Documents\\02.Contracts\\00_data\\base_vector\\bc"
-
-# keep as vect to use terra
 #aoi <- vect(file.path(basedata, "SkeenaRegionBndry.shp"))
 #aoi_sf <- st_as_sf(aoi)
 
-#basedata = "C:\\Users\\genev\\OneDrive\\Documents\\02.Contracts\\00_data\\base_vector\\regions"
 basedata = "inputs"
 
 aoi <- vect(file.path(basedata, "SkeenaRegionBndry.gpkg"))
@@ -39,8 +31,12 @@ aoi_sf <- st_as_sf(aoi)
 #PROJCS["NA_Lambert_Azimuthal_Equal_Area",GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433],METADATA["World",-180.0,-90.0,180.0,90.0,0.0,0.0174532925199433,0.0,1262]],PROJECTION["Lambert_Azimuthal_Equal_Area"],PARAMETER["False_Easting",0.0],PARAMETER["False_Northing",0.0],PARAMETER["Central_Meridian",-100.0],PARAMETER["Latitude_Of_Origin",45.0],UNIT["Meter",1.0]]
 
 # mask the raster by aoi 
+#srast <- mask(skeena, aoi)
+#srast <- crop(skeena, aoi)
+#writeRaster(srast, file.path("inputs", "sk_rast_template.tif"))
 
-srast <- mask(skeena, aoi)
+srast <- rast(file.path("inputs", "sk_rast_template.tif"))
+
 
 
 # update the bedrock layer based on geology file 
@@ -49,18 +45,26 @@ srast <- mask(skeena, aoi)
 #bcdc_search("rock")
 
 rocks <- bcdc_query_geodata("ef8476ed-b02d-4f5c-b778-0d44c9126144") |>
-  filter(INTERSECTS(aoi_sf)) |>
-  select(ROCK_TYPE_DESCRIPTION, ROCK_TYPE_CODE, ROCK_CLASS) |>
+  #filter(INTERSECTS(aoi_sf)) |>
+  #select(ROCK_TYPE_DESCRIPTION, ROCK_TYPE_CODE, ROCK_CLASS, ORIGINAL_DESCRIPTION) |>
   collect()
+
+rocks <- rocks %>% 
+  select(ROCK_TYPE_DESCRIPTION, ROCK_TYPE_CODE, ROCK_CLASS, ORIGINAL_DESCRIPTION) 
+
 
 skrocks <- rename_with(rocks, tolower) %>% 
   st_intersection(aoi_sf)%>% 
-  select(rock_type_description)
-  
+  select(rock_type_description, original_description)
 
-st_write(skrocks, file.path("inputs", "skeena_clip_soils.gpkg"))
+st_write(skrocks, file.path("inputs", "skeena_clip_soils.gpkg"), append = FALSE)
+
+skrocks <- st_read(file.path("inputs", "skeena_clip_soils.gpkg"))
+
 
 # merge the soild with sediment layer 
+
+# download quaternary soils sediment, union to single polygon / multipolygonb and use in QGIS 
 
 sed <- st_read(file.path("inputs", "quat_sed.gpkg")) %>%
   mutate(rock_type_description = "quaternary sediment") %>% 
@@ -70,12 +74,15 @@ st_write(sed, file.path("inputs", "quad_sed_simple.gpkg"))
 
 
 
-# in QGIS use symetrical difference to remove sediment areas within the bedrock layer & UNION   WITH BEDROCK 
+# in QGIS use symmetrical difference to remove sediment areas within the bedrock layer & UNION   WITH BEDROCK 
 
-br <- st_read(file.path("inputs", "sk_bedrock_sediment_raw.gpkg")) %>% 
-  select(rock_type_description)
+br <- st_read(file.path("inputs", "sk_bedrock_sediment_raw.gpkg")) 
+br <- br %>% 
+  mutate(rock_type_description  = case_when(
+    is.na(rock_type_description) ~ "quaternary sediment", 
+    TRUE ~ as.character(rock_type_description))) %>% 
+  select(c(-rock_type_description_2, -layer))
 
-skrocks <- br
 
 # read in Paulas key and make a unique id values
 bedrock_key <- read_excel(file.path("inputs","Skeena_Geology for land facets April 2_2024.xlsx"), 
@@ -83,62 +90,112 @@ bedrock_key <- read_excel(file.path("inputs","Skeena_Geology for land facets Apr
   rename("rock_type" = Rock_type_description.,
          "rock_class" = Rock.class.,
          "rock_class_det" = Groups.for.land.facet.analysis)%>%
-  select(rock_type, rock_class, rock_class_det)
+  select(rock_type, rock_class, rock_class_det) %>% 
+  mutate(rock_class_det = tolower(rock_class_det))
 
+# merge the updated bedrock layer with the bedrock key 
 
-rock_desc_class <- unique(bedrock_key$rock_type) #50
-rock_class_class <- unique(bedrock_key$rock_class) #5
-rock_class_det_class <- unique(bedrock_key$rock_class_det) #19 
-
-
-rock_class_id <- bedrock_key %>%
-  select(rock_class) %>%
-  distinct() %>%
-  mutate(rock_class_no = seq(1,length(unique(rock_class)),1))
+#rock_desc_class <- unique(bedrock_key$rock_type) #50
+#rock_class_class <- unique(bedrock_key$rock_class) #5
+#rock_class_det_class <- unique(bedrock_key$rock_class_det) #19 
 
 rock_class_det_id <-  bedrock_key %>% 
   select(rock_class_det) %>%
+  mutate(rock_class_det = tolower(rock_class_det)) %>%
   distinct() %>%
   mutate(rock_class_det_no = seq(1,length(unique(rock_class_det)),1))
 
-bedrock_key <- left_join(bedrock_key, rock_class_id ) %>% 
-  left_join(rock_class_det_id) %>%
-   rename("rock_type_description" = rock_type )
+
+# create a key that matched the details with a numeric number 
+bk_key <- left_join(bedrock_key,rock_class_det_id) %>% 
+  rename("rock_type_description" = rock_type) %>% 
+  mutate(rock_type_description  = tolower(rock_type_description),
+         rock_class_det = tolower(rock_class_det))%>%
+  mutate(rock_type_description = case_when(
+    rock_type_description == "metamorphic rocks, undivided (metavolcanics and metasediments)" ~ "metamorphic rocks, undivided",
+    TRUE ~ as.character(rock_type_description)))
+
+bk_key_out <- bk_key %>% 
+  select(rock_class_det, rock_class_det_no)%>% 
+  distinct()
+
+#write.csv(bk_key_out, file.path("inputs", "bedrock_key.csv"))
+
+#ubr <- unique(br$rock_type_description)
+#ubk <- unique(bk_key$rock_type_description)
+#setdiff(ubr, ubk)
 
 
-# see what data is in the skeena soil data and if everything matches
-# 
-# rocks_class_id <- skrocks %>% 
-#   st_drop_geometry() %>% 
-#   group_by(rock_class)%>%
-#   summarise(count = n())
-# 
-# rock_type_description <- skrocks %>% 
-#   st_drop_geometry() %>% 
-#   group_by(rock_type_description)%>%
-#   summarise(count = n())%>% 
-#   mutate(rock_type = rock_type_description)
-# 
-# rocks_class_id <- left_join(rock_type_description, bedrock_key, by = "rock_type")
-# 
+# update the undivided rocks into a detailed classifiaction 
 
-# combine the raster and key 
-#skrocks <- skrocks #%>% 
- # select(-rock_class)
+# read in the raw sheet and update the undivided categories first 
+undiv_key <- read_excel(file.path("inputs","Skeena_Geology for land facets April 2_2024.xlsx"), 
+                        sheet = "raw", .name_repair = "universal") %>% 
+  select(ROCK_TYPE_DESCRIPTION, ORIGINAL_DESCRIPTION, New.Rock_type.description) %>% 
+  filter(ROCK_TYPE_DESCRIPTION == "undivided sedimentary rocks") %>%
+  distinct()
 
-skrocks_out <- left_join(skrocks, bedrock_key, by = "rock_type_description") %>% 
-  select(-rock_class, -rock_class_no)
- # select(-id, -bedrock_unit_id, -stratigraphic_age_code, -objectid, -rock_type_code )
+# match the undivided detai;led catergory and update the br bedrock data set
+
+aa <- br %>% 
+  #select(original_description, rock_type_description) %>% 
+  #filter(rock_type_description == "undivided sedimentary rocks") %>% 
+  #st_drop_geometry() %>% 
+  left_join(undiv_key, join_by(original_description == ORIGINAL_DESCRIPTION), relationship = "many-to-many") %>% 
+  select(-ROCK_TYPE_DESCRIPTION)
+
+ab <- aa %>% 
+  mutate(rock_type_description2 = case_when(
+    !is.na(New.Rock_type.description) ~ New.Rock_type.description,
+    TRUE ~ as.character(rock_type_description))) %>%
+  select(rock_type_description2)%>%
+  mutate(rock_type_description2 = tolower(rock_type_description2)) %>% 
+  rename("rock_type_description" = rock_type_description2)
+
+br <- ab
+
+# check against key 
+ubk <- unique(bk_key$rock_type_description)
+ubr <- unique(br$rock_type_description)
+
+setdiff(ubr, ubk)
+
+unique(bk_key$rock_class_det)
+
+bb <- bk_key %>% 
+  add_row(rock_type_description  = "coarse clastic sedimentary" ,rock_class = NA, 
+          rock_class_det =  "coarse clastic sedimentary", rock_class_det_no = 10) %>% 
+  add_row(rock_type_description  = "marine sedimentary and volcanic" ,rock_class = NA, 
+        rock_class_det =  "marine sedimentary and volcanic", rock_class_det_no = 12 ) %>% 
+  add_row(rock_type_description  = "fine clastic sedimentary" ,rock_class = NA, 
+          rock_class_det ="fine clastic sedimentary", rock_class_det_no = 8) %>% 
+  add_row(rock_type_description  = "metavolcanics and metasediments" ,rock_class = NA, 
+          rock_class_det ="metavolcanics and metasediments", rock_class_det_no = 6) %>% 
+  add_row(rock_type_description  = "siliceous metamorphic"  ,rock_class = NA, 
+          rock_class_det ="siliceous metamorphic" , rock_class_det_no = 9)
+
+
+
+
+
+
+
+skrocks_out <- left_join(br, bb , by = "rock_type_description") %>% 
+  select(-rock_class )#, -rock_class_no, -original_description)
+# select(-id, -bedrock_unit_id, -stratigraphic_age_code, -objectid, -rock_type_code )
+
+
+
 
 # convert to raster 
 #rast(skrocks_out)
 
 srockvec = as(skrocks_out,"SpatVector")
 #rock_no = rasterize(srockvec, skeena, field="rock_class_no")
-rockdet_no = rasterize(srockvec, skeena, field="rock_class_det_no")
+rockdet_no = rasterize(srockvec, srast, field="rock_class_det_no")
 
 #rock_no <- mask(rock_no, aoi)
-rockdet_no<- mask(rockdet_no, aoi)
+rockdet_no<- mask(rockdet_no, srast)
 
 #writeRaster(rock_no, file.path("inputs", "sk_bedrock_class.tif"), overwrite = TRUE)
 writeRaster(rockdet_no, file.path("inputs", "sk_bedrock_class_det.tif"), overwrite = TRUE)
@@ -170,7 +227,7 @@ sk_rockclassdet_cat <- w + rockdet_no
 writeRaster(sk_rockclassdet_cat, file.path("outputs", "sk_lf_rockclassdet.tif"), overwrite = TRUE)
 
 
-srast <- rast(file.path("outputs", "sk_lf_rockclassdet.tif"))
+#srast <- rast(file.path("outputs", "sk_lf_rockclassdet.tif"))
 
 
 
@@ -218,14 +275,14 @@ ids <- ids %>%
       acc_sum > 2 & acc_sum <=4 ~ 3,
       acc_sum > 4 & acc_sum <=8 ~ 2,
       acc_sum > 8 & acc_sum <=16 ~ 1,
-      .default = as.numeric(0)
+      .default = as.numeric(1)
     ))
              
              
 
 # generate Summary tables 
 
-write_csv(  ids , file.path("outputs", "landscape_facet_summary_rcd.csv"))
+write_csv(ids , file.path("outputs", "landscape_facet_summary_rcd.csv"))
 #write_csv(ls_facet_sum_rc, file.path("outputs", "landscape_facet_summary_rc.csv"))
 
          
