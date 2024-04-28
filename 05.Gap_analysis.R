1b6ca978773a3dcbe3a9e59f4e3c0e0cd4af9621
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2024-04-28 11:37:26 -0700

    calcaulate percent protected

--- 05.Gap_analysis.R
+++ 05.Gap_analysis.R
@@ -23,24 +23,24 @@ rar <- rast(file.path("outputs", "sk_rarity_conc.tif"))
 div <- mask(div, aoi)
 rar <- mask(rar, aoi)
 
-
-# reclass only the highest values , convert to 1 and 2. 
-
-m <- c(0, 3, 0, # lowest diversity 
-       4, 4, 1,
-       5, 5, 2) # highest diversity 
-
-rclmat <- matrix(m, ncol=3, byrow=TRUE)
-
-# reclass diversity 
-div_high <- classify(div, rclmat, include.lowest=TRUE)
-rar_high <- classify(rar, rclmat, include.lowest = TRUE)
-
-sr <- c(div_high, rar_high)
-writeRaster(sr, file.path("outputs", "sk_high_conc_dr_stack.tif"), overwrite = TRUE)
-
-
-
+# 
+# # reclass only the highest values , convert to 1 and 2. 
+# 
+# m <- c(0, 3, 0, # lowest diversity 
+#        4, 4, 1,
+#        5, 5, 2) # highest diversity 
+# 
+# rclmat <- matrix(m, ncol=3, byrow=TRUE)
+# 
+# # reclass diversity 
+# div_high <- classify(div, rclmat, include.lowest=TRUE)
+# rar_high <- classify(rar, rclmat, include.lowest = TRUE)
+# 
+# sr <- c(div_high, rar_high)
+# writeRaster(sr, file.path("outputs", "sk_high_conc_dr_stack.tif"), overwrite = TRUE)
+# 
+# 
+# 
 
 
 
@@ -96,7 +96,7 @@ write.csv(pc_div_sum,file.path("outputs", "div_pcper_ecoregion.csv"))
 
 
 # rasterize the zones
-ecr <- rasterize(ecv, srast, field="ECOREGION_NAME")
+ecr <- rasterize(ecv, temp, field="ECOREGION_NAME")
 ex <- expanse(rar, unit="m", byValue=TRUE, zones=ecr, wide=TRUE)[,-1]
 
 write.csv(ex, file.path("outputs", "rare_per_ecoregion.csv"))
@@ -115,7 +115,7 @@ pc_rare_sum <- ex %>%
 write.csv(pc_rare_sum,file.path("outputs", "rare_pcper_ecoregion.csv"))
 
 
-         
+
 
 
 
@@ -146,11 +146,61 @@ pros <- pros %>%
   mutate(protected = "protected")
 
  
+# clip to the regions 
+aoi <- st_as_sf(aoi)
+pross <- st_intersection(pros, aoi) 
+
+st_write(pross, file.path("outputs", "protected_lands_clipped.gpkg"))
+
+
+
 
 ## what proportion of the skeena region is currently protected? 
 
+# how much area is protected within each eco_region: ()
+
+# simplify protected area 
+pross_u <- pross %>% select(protected)
+pro_ecoreg <- st_intersection( ec, pross_u) 
+pro_ecoreg <- pro_ecoreg |> 
+  mutate(pro_area = st_area(pro_ecoreg))
+
+
+pro_sum <- pro_ecoreg %>%
+  st_drop_geometry() |> 
+  group_by(ECOREGION_NAME) |> 
+  mutate(prosum = sum(pro_area)) %>%
+  select(-protected, -pro_area, -area_m2) |> 
+  distinct()
+
+ecsum <- ec %>%
+  st_drop_geometry() |> 
+  group_by(ECOREGION_NAME) |> 
+  mutate(totsum = sum(area_m2))
+
+
+ecpro <- left_join(pro_sum, ecsum)%>%
+  group_by(ECOREGION_NAME) |> 
+  mutate(pc_pro = (prosum/totsum)*100)
+
+library(readr)
+write_csv(ecpro, file.path("outputs", "protection_per_ecoregion.csv"))
+
+
+
+
+
+
+
+
+
+
+
+
 # rasterize the zones
-pror <- rasterize(pros, srast, field="protected")
+pror <- rasterize(pross, temp, field="protected")
+
+
 
 #diversity
 div_ex <- expanse(div, unit="m", byValue=TRUE, zones=pror, wide=TRUE)[,-1]
