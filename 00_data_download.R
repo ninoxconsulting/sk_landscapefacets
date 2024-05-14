b4593bc5c9043dc6ed2167b095e5081b9ddd9c0a
gcperk <genevieve.c.perkins@gmail.com>
2024-05-14 16:12:14 -0700

    update concentration thresholds

--- 00_data_download.R
+++ 00_data_download.R
@@ -9,7 +9,7 @@ library(sf)
 
 srast <- rast(file.path("inputs", "sk_rast_template.tif"))
 
-in_aoi<- st_read(file.path("inputs", "sk_poly_template.gpkg"))
+in_aoi <- st_read(file.path("inputs", "sk_poly_template.gpkg"))
 
 #in_aoi <- st_as_sf(in_aoi)
 
@@ -19,19 +19,19 @@ in_aoi<- st_read(file.path("inputs", "sk_poly_template.gpkg"))
 
 # 1. lakes
 #https://catalogue.data.gov.bc.ca/dataset/b3f58ed8-376f-4962-9657-36297a5f41cf
-
-lakes <- bcdc_query_geodata("cb1e3aba-d3fe-4de1-a2d4-b8b6650fb1f6") |>
-  # filter(INTERSECTS(aoi_sf)) |> 
-  select( WATERBODY_TYPE, AREA_HA) |>
-  collect()
-
-lakes1 <- sf::st_intersection(lakes, in_aoi) |> 
-  select( WATERBODY_TYPE, AREA_HA) %>% 
-  filter(AREA_HA > 1) 
-
-
-st_write(lakes1, file.path("inputs", "lakes.gpkg"), append = FALSE)
-
+# 
+# lakes <- bcdc_query_geodata("cb1e3aba-d3fe-4de1-a2d4-b8b6650fb1f6") |>
+#   # filter(INTERSECTS(aoi_sf)) |> 
+#   select( WATERBODY_TYPE, AREA_HA) |>
+#   collect()
+# 
+# lakes1 <- sf::st_intersection(lakes, in_aoi) |> 
+#   select( WATERBODY_TYPE, AREA_HA) %>% 
+#   filter(AREA_HA > 1) 
+# 
+# 
+# st_write(lakes1, file.path("inputs", "lakes.gpkg"), append = FALSE)
+# 
 
 # rivers 
 
@@ -127,12 +127,51 @@ st_write(cons, file.path("inputs", "cons_lands.gpkg"), append = FALSE)
 ec <- bcdc_query_geodata("d00389e0-66da-4895-bd56-39a0dd64aa78") |>
   collect()
 
-st_write(ec , file.path("inputs", "bc_ecoreg.gpkg"), append = FALSE)
+#st_write(ec , file.path("inputs", "bc_ecoreg.gpkg"), append = FALSE)
+ec <- st_read(file.path("inputs", "bc_ecoreg.gpkg"))
+
 
 ec <- sf::st_intersection(ec, in_aoi)%>% 
-  select(ECOREGION_CODE,  ECOREGION_NAME)
+  select(ECOREGION_CODE,  ECOREGION_NAME)%>%
+  st_intersection(in_aoi)
 
 st_write(ec , file.path("inputs", "sk_ecoreg.gpkg"), append = FALSE)
+
+
+
+# TAP Old growth 
+## intact watersheds - From TAP old growth 
+#https://catalogue.data.gov.bc.ca/dataset/b684bdbf-8824-4bc6-8d22-329d9b97c043
+
+list.files(file.path('/home/user/Documents/00_data/base_vector/bc/BC_TAP_Forestry_data/BCVW_tap_watewrshed_data/'))
+
+iw <- rast(file.path('/home/user/Documents/00_data/base_vector/bc/BC_TAP_Forestry_data/BCVW_tap_watewrshed_data/',"Map6_IntactWS_2021_08_08.tif"))
+template <-rast(file.path("inputs", "sk_rast_template.tif"))
+iw <- resample(iw, template)
+iw_sk <- mask(iw, template)
+
+# MIGHT WANT TO REORDER TO SELECT WHERE vALUE >7 ETC 
+writeRaster(iw_sk, file.path("inputs", "TAP_intact_watershed.tif"), overwrite = TRUE)
+
+
+# BIG TREES 
+bt<- rast(file.path('/home/user/Documents/00_data/base_vector/bc/BC_TAP_Forestry_data/BCVW_tap_watewrshed_data/',"Map2_BigTreeOG_2021_10_24.tif"))
+#bt <- project(bt, template)
+#template <-rast(file.path("inputs", "sk_rast_template.tif"))
+bt <- resample(bt, template)
+bt_sk <- mask(bt, template)
+
+writeRaster(bt_sk , file.path("inputs", "TAP_bigtrees_raw.tif"), overwrite = TRUE)
+
+# ancient forests
+bt <- rast(file.path('/home/user/Documents/00_data/base_vector/bc/BC_TAP_Forestry_data/BCVW_tap_watewrshed_data/',"Map4_Ancient_forest_2021_07_22.tif"))
+#bt <- project(bt, template)
+#template <-rast(file.path("inputs", "sk_rast_template.tif"))
+bt <- resample(bt, template)
+bt_sk <- mask(bt, template)
+
+writeRaster(bt_sk , file.path("inputs", "TAP_ancient_forest_raw.tif"), overwrite = TRUE)
+
 
 
 
@@ -208,3 +247,43 @@ rrrreg <- sf::st_intersection(rreg,  in_aoi)
 
 st_write(rrrreg, file.path("inputs", "eaubc_reg.gpkg"), append = FALSE)
 
+
+
+
+
+## fine scale intact 
+
+# critcal habitat - federal 
+#https://catalogue.data.gov.bc.ca/dataset/critical-habitat-for-federally-listed-species-at-risk-posted-
+
+cri <- bcdc_query_geodata("076b8c98-a3f1-429b-9dae-03faed0c6aef") |>
+  select(SCIENTIFIC_NAME, COMMON_NAME_ENGLISH, CRITICAL_HABITAT_STATUS, LAND_TENURE)
+  collect()
+  
+cri <- cri %>%  
+  select(SCIENTIFIC_NAME, COMMON_NAME_ENGLISH, CRITICAL_HABITAT_STATUS, LAND_TENURE)%>%
+  st_intersection(in_aoi)
+
+st_write(cri, file.path("inputs", "fed_listed_sp_raw.gpkg"))
+
+
+
+
+
+
+#  Species and Ecosystems at Risk - Publicly Available
+#https://catalogue.data.gov.bc.ca/dataset/species-and-ecosystems-at-risk-publicly-available-occurrences-cdc
+
+eco <- bcdc_query_geodata("0e035e55-f257-458f-9a96-80c01c69d389") |>
+  select(SCI_NAME, ENG_NAME, EL_TYPE, PROV_RANK, BC_LIST, RANK, RANK_DESC) %>%
+  collect()
+
+eco <- eco %>%
+  select(SCI_NAME, ENG_NAME, EL_TYPE, PROV_RANK, BC_LIST, RANK, RANK_DESC) %>%
+  st_intersection(in_aoi)%>% 
+  filter(BC_LIST %in% c("Red", "Blue"))
+    
+st_write(eco, file.path("inputs", "bc_red_blue_sp_raw.gpkg"))
+
+
+## might want to further filter the reliability of the area via rqnk description/code/ Need to check with Paula.
