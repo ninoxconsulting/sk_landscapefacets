a28c70eca87a33b421a3523ad969d8cc774aef17
joshuasamuelevans <j_evans@ucalgary.ca>
2024-05-16 18:14:00 -0700

    Code for creating median composite and thresholding on NPP layer and beginning the connectivity piece

--- 04_ndvi.R
+++ 04_ndvi.R
@@ -9,6 +9,10 @@ library(sf)
 library(ggplot2)
 library(dplyr)
 library(readr)
+library(ggspatial)
+library(tidyterra)
+library(cowplot)
+
 
 template <- rast(file.path("inputs", "sk_rast_template.tif"))
 
@@ -22,8 +26,96 @@ writeRaster(ndvi, file.path("inputs", "ndvi_skeena_comp_aligned.tif"))
 
 
 # create a moving window in qgis and then 
-
 ndvi <- rast(file.path("inputs", "ndvi_sk_mean_101c.tif"))
 ndvi <- mask(ndvi, template)
 
 writeRaster(ndvi, file.path("outputs", "ndvi_final.tif"))
+
+
+# Josh starts here
+# read in raster
+ndvi_2019 <- rast(file.path("..", "inputs", "ndvi_skeena_2019.tif"))
+ndvi_2020 <- rast(file.path("..", "inputs", "ndvi_skeena_2020.tif"))
+ndvi_2021 <- rast(file.path("..", "inputs", "ndvi_skeena_2021.tif"))
+ndvi_2022 <- rast(file.path("..", "inputs", "ndvi_skeena_2022.tif"))
+ndvi_2023 <- rast(file.path("..", "inputs", "ndvi_skeena_2023.tif"))
+
+npp <- rast(file.path("..", "inputs", "npp_skeena_2019_2023.tif"))
+template <- rast(file.path("..", "inputs", "sk_rast_template.tif"))
+
+# resample ndvi to the template
+sprc <- sprc(ndvi_2019, ndvi_2020, ndvi_2021, ndvi_2022, ndvi_2023, npp)
+
+sprc <- impose(sprc, template)
+
+npp <-  sprc[[6]]
+sprc <- sprc[[1:5]]
+
+# take the median of the ndvi 
+median <- app(sprc, fun="median")
+
+
+rescale <- function(x) {
+  
+  (x - global(x, "min", na.rm = T) %>% pull()) / 
+  (global(x, "max", na.rm = T) %>% pull() - global(x, "min", na.rm = T) %>% pull)
+  
+}
+
+ndvi_scale <- rescale(median)
+
+# write out the median and scaled products 
+
+writeRaster(median, file.path("..", "primary_productivity", "ndvi_2019_2023.tif"))
+
+writeRaster(ndvi_scale, file.path("..", "primary_productivity", "ndvi_2019_2023_scaled.tif"))
+
+npp_scale <- rescale(npp)
+
+# I am going to stretch the npp layer 
+npp_stretch <- stretch(npp_scale, minq = 0.97, maxq = 1)
+
+npp_stretch <- rescale(npp_stretch)
+
+npp_stretch <- subst(npp_stretch, NA, 0)
+
+added <- rescale(npp_stretch + ndvi_scale)
+
+
+ndvi <- ggplot() +
+  geom_spatraster(data = ndvi_scale) +
+  # Use the palette you like, in this case:
+  scale_fill_whitebox_c( palette = "viridi") +
+  theme_void() +
+  labs(title = "NDVI")
+
+npp <- ggplot() +
+  geom_spatraster(data = npp_scale) +
+  # Use the palette you like, in this case:
+  scale_fill_whitebox_c( palette = "viridi") +
+  theme_void() +
+  labs(title = "NPP")
+
+
+npp_stretch <- ggplot() +
+  geom_spatraster(data = npp_stretch) +
+  # Use the palette you like, in this case:
+  scale_fill_whitebox_c( palette = "viridi") +
+  theme_void() +
+  labs(title = "NPP Stretched (0.97%)")
+
+
+add <- ggplot() +
+  geom_spatraster(data = added) +
+  # Use the palette you like, in this case:
+  scale_fill_whitebox_c( palette = "viridi") +
+  theme_void() +
+  labs(title = "NDVI + NPP")
+
+cowplot::plot_grid(ndvi, npp, npp_stretch, add, align = "hv")
+
+# writing out the added product
+writeRaster(added, file.path("..", "primary_productivity", "ndvi_npp_scaled_2019_2023.tif"), overwrite = T)
+
+rstudio.prefs::use_rstudio_prefs(editor_theme = 'Idle Fingers', indend_guides = 'rainbowlines')
+
--- 07_bulkley_valley_case_study.R
+++ 07_bulkley_valley_case_study.R
@@ -5,15 +5,16 @@ library(terra)
 library(sf)
 library(tidyverse)
 library(raster)
+library(tictoc)
 
 # read in diversity for template raster
 
-template <- rast('C:/Users/j_evans/Downloads/sk_diversity_conc.tif')
+template <- rast(file.path("..", "inputs", "sk_rast_template.tif"))
 
-test_patches <- st_read('F:/Other/Nixon/landscape_facets/connectivity/clipped_area.gpkg')
+
+test_patches <- sf::st_read("F:/Other/Nixon/landscape_facets/inputs/skeena_clip_ce_bulkley.gpkg")
 
 # clipping the template to the test patches then rasterizing
-
 template_c <- crop(template, test_patches)
 
 # rasterizing
@@ -24,37 +25,133 @@ patches <- raster(patches)
 # creating the MPG
 patchyMPG <- MPG(patches, patch = (patches ==1 ))
 
+# plotting the MPG
 plot(patchyMPG, quick = "mpgPlot", theme = FALSE)
 
-scalarAnalysis <- threshold(patchyMPG, nThresh = 5)
-## Use kable to render this as a table
-scalarAnalysis$summary
+# Lets plot the thresholds at different thresholds to see where we 
+# see the amount of information cease to change
 
-scalarAnalysis <- threshold(patchyMPG, nThresh = 100)
-ggplot(scalarAnalysis$summary, aes(x = maxLink, y = nComponents)) +
-  geom_line(colour = "forestgreen") +
-  xlab("Link Threshold (resistance units)") +
-  ylab("Number of components") +
-  scale_x_continuous(breaks = seq(0, 1000, by = 100)) +
-  scale_y_continuous(breaks = 1:20) +
-  theme_light() +
-  theme(axis.title = element_text())
 
-ggplot() +
-  geom_raster(
-    data = ggGS(patchyMPG, "patchId"),
-    aes(x = x, y = y, fill = value > 0)
-  ) +
-  scale_fill_manual(values = "grey") +
-  geom_segment(
-    data = ggGS(patchyMPG, "links"),
-    aes(
-      x = x1, y = y1, xend = x2, yend = y2,
-      colour = lcpPerimWeight >= 250
+for(i in seq(10, 100, 10)) {
+  
+  scalarAnalysis <- threshold(patchyMPG, nThresh = i)
+  
+  g <- ggplot(scalarAnalysis$summary, aes(x = maxLink, y = nComponents)) +
+    geom_line(colour = "forestgreen") +
+    xlab("Link Threshold (resistance units)") +
+    ylab("Number of components") +
+    labs(title = paste("threshold = ", i)) +
+    theme_light() +
+    theme(axis.title = element_text())
+  # we can see that having 100 thresholds is more than what is necessary to capture the 
+  # variation in the landscape with the graph plateuing at 30 components. Lets redo this with 30
+  # thresholds
+  print(g)
+  
+}
+
+# threshold around 50 seems reasonable to go with
+
+# showing the components
+
+## Use the grainscape::threshold() function to create a new network
+## by thresholding links
+fragTh <- threshold(patchyMPG, nThresh = 50)
+
+# mapping the alpha_centrality at every scale:
+
+
+
+centrality <- map(10:50, possibly(function(x) {
+
+  
+    print(x)
+    ## Find the components in that thresholded network using
+    ## an igraph package function
+    fragThC <- components(fragTh$th[[x]])
+    ## Extract the node table and append the
+    ## component membership information
+    fragThNodes <- data.frame(vertex_attr(fragTh$th[[x]]),
+                              component = fragThC$membership
     )
-  ) +
-  scale_colour_manual(values = c("forestgreen", NA)) +
-  geom_point(
-    data = ggGS(patchyMPG, "nodes"), aes(x = x, y = y),
-    colour = "darkgreen"
-  )
+    
+    ## Assess centrality of a thresholded network
+    ## made in the previous example (threshold = 20)
+    fragThDegree <- try(alpha_centrality(fragTh$th[[x]]))
+    ## Add degree to the node table
+    fragThNodes <- data.frame(vertex_attr(fragTh$th[[x]]), centrality = fragThDegree) %>%
+      mutate(threshold = x)
+    
+    fragThNodes$x <- fragThNodes$centroidX
+    fragThNodes$y <- fragThNodes$centroidY
+    
+    return(fragThNodes)
+
+    
+})) %>%
+  keep(~ !is.null(.x))
+  
+
+files <- list()
+# plot each as a function of node size:
+for (i in seq_along(centrality)) {
+  
+  print(i)
+  
+  figure <- ggplot() +
+    geom_raster(
+      data = ggGS(patchyMPG, "patchId"),
+      aes(x = x, y = y, fill = value > 0)
+    ) +
+    scale_fill_manual(values = "grey") +
+    geom_segment(
+      data = ggGS(patchyMPG, "links"),
+      aes(
+        x = x1, y = y1, xend = x2, yend = y2,
+        colour = lcpPerimWeight > 20
+      )
+    ) +
+    scale_colour_manual(values = c("forestgreen", NA)) +
+    geom_point(
+      data = centrality[[i]],
+      aes(x = x, y = y, size = centrality), colour = "darkgreen"
+    ) +
+    ggtitle(paste("Node importance metrics thresh = ", i))
+  
+  filename <- paste0('F:/Other/Nixon/landscape_facets/connectivity/images/centrality_thresh_', i, ".png")
+  
+  files[[i]] <- filename
+  
+  ggsave(filename, plot = figure, width = 862, height = 551, units = "px", dpi = 150)
+  
+}
+
+
+gif_file <- "F:/Other/Nixon/landscape_facets/connectivity/node_importance.gif"
+gifski(unlist(files), gif_file, 862, 551, delay = 0.5)
+
+utils::browseURL(gif_file)
+
+
+
+print(figure)
+
+# building a grains of connectivity analysis 
+# Use 100 thresholds 
+tic()
+patchyGOC <- GOC(patchyMPG, nThresh = 50)
+toc() # 94 seconds
+
+
+
+# lets plot the connected regions at various thresholds:
+for(i in seq(1, 50, 5)) {
+  
+  g <- plot(grain(patchyGOC, whichThresh = i), quick = "grainPlot", theme = FALSE) +
+    ggtitle(paste("threshold = ", i))
+  
+  print(g)
+  
+}
+
+#
