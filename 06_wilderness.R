a1e9a4ae4c95ba4e73c8050698afe1bde9fe7287
gcperk <genevieve.c.perkins@gmail.com>
2024-06-25 14:08:07 -0700

    update wilderness

--- 06_wilderness.R
+++ 06_wilderness.R
@@ -1,25 +1,24 @@
 # wilderness
 
-
-library(bcdata)
 library(dplyr)
 library(terra)
 library(sf)
 
-
-# read in aoi + prepped areas 
-
-#basedata = "C:\\Users\\genev\\OneDrive\\Documents\\02.Contracts\\00_data\\base_vector\\regions"
-basedata = "inputs"
-
-aoi <- vect(file.path(basedata, "SkeenaRegionBndry.gpkg"))
-aoi_sf <- st_as_sf(aoi)
-
-srast = terra::rast(file.path("outputs","facet_rcd_diversity_101c.tif"))
+# read in templates 
+srast <- rast(file.path("inputs", "sk_rast_template.tif"))
+in_aoi <- read_sf(file.path("inputs", "sk_poly_template.gpkg"))
 
 
 dis <- st_read(file.path("inputs", "skeena_clip_ce.gpkg" ))
 rds <- st_read(file.path("inputs", "skeena_clippoly_roads.gpkg" ))
+
+
+private <- st_read(file.path("inputs",  "sk_privateland_raw.gpkg"))
+
+"cutblocks"
+
+
+
 
 
 dis <- dis %>% 
@@ -37,20 +36,7 @@ human <- rasterize(dist, srast, field = "type")
 
 explot(human)
 
-
-## from-to-becomes
-m <- c(0, 1, 1,
-       1, 2, 2,
-       2, 3, 3,
-       3, 4, 4,
-       4, 6, 5)
-rclmat <- matrix(m, ncol=3, byrow=TRUE)
-rc <- classify(con_rare, rclmat, include.lowest=TRUE)
-
-writeRaster(rc, file.path("outputs", "sk_rarity_conc.tif"))
-
-
-
+# read in the private lands 
 
 
 
