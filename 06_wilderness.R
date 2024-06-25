002da7b9cef65243f5eaf785e9e75f923b8b1a24
gcperk <genevieve.c.perkins@gmail.com>
2024-06-25 14:59:47 -0700

    generate wilderness areas

--- 06_wilderness.R
+++ 06_wilderness.R
@@ -8,35 +8,37 @@ library(sf)
 srast <- rast(file.path("inputs", "sk_rast_template.tif"))
 in_aoi <- read_sf(file.path("inputs", "sk_poly_template.gpkg"))
 
-
-dis <- st_read(file.path("inputs", "skeena_clip_ce.gpkg" ))
-rds <- st_read(file.path("inputs", "skeena_clippoly_roads.gpkg" ))
-
-
-private <- st_read(file.path("inputs",  "sk_privateland_raw.gpkg"))
-
-"cutblocks"
-
-
-
-
-
-dis <- dis %>% 
+dis <- st_read(file.path("inputs", "skeena_clip_ce.gpkg" ))%>%
+  filter(CEF_HUMAN_DISTURB_FLAG =="Human Disturb Current 20yr")%>%
   mutate(type = 1) %>% 
   select(type)
 
-rds <- rds %>% 
+rds <- st_read(file.path("inputs", "skeena_clippoly_roads.gpkg" ))%>% 
+  mutate(type = 1) %>% 
+  select(type)
+
+
+private <- st_read(file.path("inputs",  "sk_privateland_raw.gpkg"))%>%
+  filter(OWNER_TYPE == "Private") %>%
   mutate(type = 1) %>% 
   select(type)
 
 
 dist <- bind_rows(rds, dis)
 
-human <- rasterize(dist, srast, field = "type")
+dist <- rbind(dist, private)
 
-explot(human)
+dist <- vect(dist)
+human <- terra::rasterize(dist, srast, field = "type")
 
-# read in the private lands 
+plot(human)
+
+h <- subst(human, NA, 0)
+h <-  mask(h, srast)
+
+
+terra::writeRaster(h,file.path("outputs", "sk_wilderness.tif"), overwrite = TRUE)
+
 
 
 
