49343904e5bea731275427486ab2be04081b13e7
gcperk <genevieve.c.perkins@gmail.com>
2024-06-27 16:51:27 -0700

    add common landscape facets

--- 02_rarity2.R
+++ 02_rarity2.R
@@ -6,6 +6,10 @@ library(ggplot2)
 library(dplyr)
 library(readr)
 
+# read in study area 
+aoi <- rast(file.path("inputs", "sk_rast_template.tif"))
+temp <- st_read(file.path("inputs", "sk_poly_template.gpkg"))
+
 srast = terra::rast(file.path("outputs", "sk_lf_barcode.tif"))
 
 # Read in the skeena facets 
@@ -266,5 +270,137 @@ writeRaster(rc, file.path("outputs", "sk_rarity_conc.tif"), overwrite = TRUE)
 # 
 # writeRaster(rc, file.path("outputs", "sk_rarity_concV2.tif"), overwrite = TRUE)
 # 
+
+
+
+
+
+
+
+## MOST COMMON bARCODES
+
+## Calculate the most common bar code types for 1) all of skeena region and 2) per ecoregion 
+
+# 1 ) skeena 
+
+# read in the rare csv file 
+rare <- read.csv(file.path("outputs", "lf_barcode_summary.csv")) 
+rr = terra::rast(file.path("outputs", "sk_lf_barcode.tif"))
+
+# Read in the skeena facets and update the most common barcodes >50% by land area 
+
+common <- rare %>% 
+  mutate(rare_id = case_when(
+    acc_sum >= 50 ~ 1,
+    .default = as.numeric(0)
+  ))
+
+# assign rarity class
+class1 <-common %>% filter(rare_id == 1) %>% pull(layer1)
+
+length(class1)
+
+uvr <- as.vector(unique(values(rr)))
+
+# check if needs class 1: 
+
+if(any(unique(uvr %in% class1)) == TRUE){
+  print("reclass values")
+  
+  for(i in class1){
+    #  i = class1[1]
+    print(i)
+    rr <- subst(rr, i, 1)
+    
+  }
+  
+}else {
+  print("no class1 reclass needed")
+}
+
+reclass <- as.vector(unique(values(rr)))
+sort(reclass)
+
+# classify the values into groups 
+
+m <- c(1, 1, 1,
+       2, 999999, 0)
+rclmat <- matrix(m, ncol=3, byrow=TRUE)
+common <- classify(rr, rclmat, include.lowest=TRUE)
+
+unique(values(common))
+rc1 <- mask(common, aoi )
+terra::writeRaster(rc1,file.path("outputs", "sk_entire_common.tif"), overwrite = TRUE)
+
+
+### 
+
+# read in study area 
+
+ec <- st_read(file.path("outputs", "sk_ecoreg_reduced.gpkg"))
+
+rr = terra::rast(file.path("outputs", "sk_lf_barcode.tif"))
+rr_poly <-  as.polygons(rr, na.rm=FALSE)
+rr_sf <- st_as_sf(rr_poly)
+
+bc_ec <- st_intersection(rr_sf, ec) 
+
+bc_ec <- bc_ec %>% 
+  mutate(area_type = st_area(.))%>% 
+  filter(!is.na(lyr.1))
+
+st_write(bc_ec, file.path("outputs", "sk_lf_barcode_ecoreg_poly.gpkg"), append = FALSE)
+
+
+ec <- ec %>% 
+  mutate(ec_area_type = st_area(.))
+
+
+# calculate the barcodes most common for each of the ecoregions 
+
+comm <- bc_ec %>% 
+  st_drop_geometry() %>% 
+  left_join(ec)%>% 
+  select(-geom) %>% 
+  rowwise() %>% 
+  mutate(pc = (area_type /ec_area_type)*100) %>% 
+  select(-area_type, -ec_area_type)%>%
+  ungroup()%>%
+  arrange(pc)
+
+library(plyr)
+ccc <- ddply(comm,.(ECOREGION_NAME),transform,csum=cumsum(pc))
+ccc <- as_tibble(ccc)%>% 
+  mutate(pc = as.numeric(pc), 
+         csum = as.numeric(csum))
+
+common <- ccc %>% 
+  mutate(rare_id = case_when(
+    csum >= 50 ~ 1,
+    .default = as.numeric(0)
+  ))
+
+comm_sf <- left_join(bc_ec, common )%>% 
+  filter(-area_type)
+
+
+st_write(comm_sf, file.path("outputs", "sk_lf_barcode_ecoreg_detail_poly.gpkg"), append = FALSE)
+
+
+### UP TO HERE 
+
+# need to calculate number of barcodes per ecoregion 
+# overlay the protected areas with the most common to see how much is protected. 
+
+# 
+# 
+# # generate Summary tables 
+# write_csv(rare , file.path("outputs", "lf_barcode_summary.csv"))
+
+
+
+
+
+
 
 
