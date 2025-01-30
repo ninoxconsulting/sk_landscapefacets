d71196b8d6cd94c3d93f4ed734da83f4d7c8afb5
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2025-01-29 17:31:05 -0800

    update to 1km raster size

--- 21_sites_raster_prep.R
+++ 21_sites_raster_prep.R
@@ -7,55 +7,68 @@ library(sf)
 
 # read in templates 
 srast <- rast(file.path("inputs", "sk_rast_template.tif"))
+srast <- aggregate(srast, fact = 10)
+names(srast) <- "template"
+
+
 in_aoi <- read_sf(file.path("inputs", "sk_poly_template.gpkg"))
 outputs <- file.path("outputs", "final", "sites", "raw_tiffs")
 
-
+#dim(srast)
+#ncell(srast)
 
 
 
 
 # read in the wilderness layer / human footprint layer
 
-w <- rast(file.path("outputs", "sk_wilderness_2023.tif"))
+w <- rast(file.path("outputs", "final", "sk_wilderness_2023.tif"))
 names(w)<- "humanfootprint"
-
+w  <- aggregate(w , fact=10, fun="mean")
 writeRaster(w, file.path(outputs, "humanfootprint.tif"), overwrite=TRUE)
 
 
 
 
 # productivity 
-
 gdd <- rast(file.path("inputs", "gdd_sk.tif"))
 names(gdd)<- "gdd"
+gdd  <- aggregate(gdd , fact=10, fun="mean")
 writeRaster(gdd, file.path(outputs, "gdd_c.tif"), overwrite=TRUE)
 
+# ndvi
 ndvi <- rast(file.path("outputs", "ndvi_2019_2023_mean_101c.tif"))
 names(ndvi)<- "ndvi"
-
+ndvi  <- aggregate(ndvi , fact=10, fun="mean")
 # might need to stack? 
-writeRaster(ndvi, file.path(outputs, "ndvi_c.tif"))
+writeRaster(ndvi, file.path(outputs, "ndvi_c.tif"), overwrite = TRUE)
 
 
+#npp 
+npp <- rast(file.path("inputs", "npp_skeena_2019_2023.tif"))
+names(npp)<- "npp"
+npp <- project(npp,srast)
+writeRaster(npp, file.path(outputs, "npp.tif"), overwrite = TRUE)
 
 
 # resistence / connectivity 
 res <- rast(file.path("inputs", "pither_resistance.tif"))
 names(res) = "resistance"
-
-writeRaster(res, file.path(outputs, "resistance_c.tif"))
+res <- aggregate(res , fact=10, fun="mean")
+writeRaster(res, file.path(outputs, "resistance_c.tif"), overwrite = TRUE)
 
 
 # microrefugia 
 mic <- rast(file.path("inputs", "microrefugia.tif"))
 names(mic) = "microrefugia"
-writeRaster(mic, file.path(outputs, "microrefugia.tif"))
+mic <- aggregate(mic  , fact=10, fun="mean")
+writeRaster(mic, file.path(outputs, "microrefugia.tif"), overwrite = TRUE)
 
 # macrorefugia
 mac <- rast(file.path("inputs", "2080s_macrorefugia.tif"))
 names(mac) = "macrorefugia"
-writeRaster(mac, file.path(outputs, "macrorefugia.tif"))
+mac <- aggregate(res , fact=10, fun="mean")
+writeRaster(mac, file.path(outputs, "macrorefugia.tif"), overwrite = TRUE)
 
 
 
@@ -63,24 +76,65 @@ writeRaster(mac, file.path(outputs, "macrorefugia.tif"))
 
 #protected _lands 
 pro <- st_read(file.path("outputs", "sk_protected_lands.gpkg"))
-pro <- rasterize(pro, srast)
+pro <- rasterize(vect(pro), srast)
 names(pro) = "protected_lands"
-writeRaster(pro, file.path(outputs, "protected_lands.tif"))
+writeRaster(pro, file.path(outputs, "protected_lands.tif"), overwrite = TRUE)
 
 
+
+
+
+# break this into species Presence/absence
 
 # red and blue sp and federal listed species. 
 rb <- st_read(file.path("inputs", "bc_red_blue_sp_raw.gpkg"))
-rb <- rasterize(rb, srast)
-names(rb) = "red_blue_sp"
-writeRaster(rb, file.path(outputs, "red_blue_sp.tif"))
+
+#unique(rb$SCI_NAME)
+#unique(rb$EL_TYPE)
+
+sp <- unique(rb$SCI_NAME)
+
+purrr::map(sp, function(x){
+  print(x)
+  rbb <- rb |> filter(SCI_NAME == x)
+  comm_name <- gsub(" ", "_", unique(rbb$ENG_NAME))
+  comm_name <- gsub("/", "", comm_name)
+  comm_name <- gsub("-", "", comm_name)
+  
+  
+  sciname <- gsub(" ", "_", unique(rbb$SCI_NAME))
+  sciname <- gsub("/", "", sciname)
+  sciname <- gsub("-", "", sciname)
+  
+  rbb <- rasterize(rbb, srast)
+  names(rbb) = paste0("bc_listed_", comm_name )
+  writeRaster(rbb, file.path(outputs, "species", paste0("bc_listed_", sciname, ".tif")), overwrite = TRUE)
+  
+})
+
+
+# rb <- rasterize(rb, srast)
+# names(rb) = "red_blue_sp"
+# writeRaster(rb, file.path(outputs, "red_blue_sp.tif"), overwrite = TRUE)
+
 
 # fed listed species
-
 rb <- st_read(file.path("inputs", "fed_listed_sp_raw.gpkg"))
-rb <- rasterize(rb, srast)
-names(rb) = "fed_listed_sp"
-writeRaster(rb, file.path(outputs, "fed_listed_sp.tif"))
+
+sp <- unique(rb$SCIENTIFIC_NAME)
+
+purrr::map(sp, function(x){
+  print(x)
+  rbb <- rb |> filter(SCIENTIFIC_NAME == x)
+  comm_name <- gsub(" ", "_", unique(rbb$COMMON_NAME_ENGLISH))
+  sciname <- gsub(" ", "_", unique(rbb$SCIENTIFIC_NAME))
+  rbb <- rasterize(rbb, srast)
+  names(rbb) = paste0("fed_listed_", comm_name )
+  writeRaster(rbb, file.path(outputs, "species", paste0("fed_listed_", sciname, ".tif")), overwrite = TRUE)
+  
+})
+
+
 
 
 # KBAs & IBAs
@@ -91,16 +145,18 @@ writeRaster(rb, file.path(outputs, "fed_listed_sp.tif"))
 
 kbat <- st_read(file.path("outputs", "final", "KBA_terrestrial.gpkg"))
 kbat <- st_transform(kbat, crs=st_crs(srast))
-pro <- rasterize(kbat, srast)
+pro <- rasterize(vect(kbat), srast)
 names(pro) = "ter_kba"
-writeRaster(pro, file.path(outputs, "kba_terrestrial.tif"))
+writeRaster(pro, file.path(outputs, "kba_terrestrial.tif"), overwrite = TRUE)
+# not included as so small 
+
 
 
 j <- st_read(file.path("outputs", "final", "Jokulhaups_sk.gpkg"))
 j <- st_transform(j, crs=st_crs(srast))
 pro <- rasterize(j, srast)
 names(pro) = "jokulhaups"
-writeRaster(pro, file.path(outputs, "jokulhaups.tif"))
+writeRaster(pro, file.path(outputs, "jokulhaups.tif"), overwrite = TRUE)
 
 #IBA 
 ib <- st_read(file.path("outputs", "final", "sk_important_bird_areas.gpkg"))
@@ -118,11 +174,13 @@ writeRaster(ib, file.path(outputs, "iba.tif"))
 
 tap <- rast( file.path("inputs", "TAP_intact_watershed.tif"))
 names(tap) = "tap_intact_watershed"
+tap <- aggregate(tap , fact=10, fun="mean")
 writeRaster(tap, file.path(outputs, "TAP_intact_watershed.tif"), overwrite = T)
 
 # big trees 
 bt <- rast(file.path("inputs", "TAP_bigtrees_raw.tif"))
 names(bt) = "tap_bigtrees"
+bt<- aggregate(bt , fact=10, fun="mean")
 writeRaster(bt, file.path(outputs, "TAP_bigtrees.tif"), overwrite = T)
 
 
@@ -149,24 +207,28 @@ writeRaster(bt, file.path(outputs, "TAP_ancient_forest.tif"), overwrite = T)
 
 # continuous measure of rarity
 con_rarec <- rast(file.path("outputs","sk_lf_rdc_rarity_101c.tif"))
+con_rarec<- aggregate(con_rarec , fact=10, fun="mean")
 names(con_rarec)= "ter_rarity"
-writeRaster(con_rarec, file.path(outputs, "ter_rarity_c.tif"))
+writeRaster(con_rarec, file.path(outputs, "ter_rarity_c.tif"), overwrite = T)
 
 # classed version
 con_rareclass <- rast(file.path("outputs", "sk_rarity_conc.tif"))
+con_rareclass <-aggregate(con_rareclass , fact=10, fun="mean")
 names(con_rareclass)= "ter_rarity_class"
-writeRaster(con_rareclass, file.path(outputs, "ter_rarity_class.tif"))
+writeRaster(con_rareclass, file.path(outputs, "ter_rarity_class.tif"), overwrite = T)
 
 
 # diversity - terrestrial 
 div= rast(file.path("outputs", "sk_lf_rdc_diversity_101c.tif"))
 names(div)= "ter_diversity"
-writeRaster(div, file.path(outputs, "ter_diversity_c.tif"))
+div<- aggregate(div , fact=10, fun="mean")
+writeRaster(div, file.path(outputs, "ter_diversity_c.tif"), overwrite = T)
 
 # classed version 
 divc <- rast(file.path("outputs", "sk_diversity_conc.tif"))
+divc<- aggregate(divc , fact=10, fun="mean")
 names(divc)= "ter_diversity_class"
-writeRaster(divc, file.path(outputs, "ter_diversity_class.tif"))
+writeRaster(divc, file.path(outputs, "ter_diversity_class.tif"), overwrite = T)
 
 
 
@@ -174,53 +236,58 @@ writeRaster(divc, file.path(outputs, "ter_diversity_class.tif"))
 
 # rarity - continuous lakes 
 rar <- rast(file.path("outputs", "sk_lakes_meanbyarea_rarity_101c.tif"))
+rar<- aggregate(rar , fact=10, fun="mean")
 names(rar )= "aq_lakes_rarity_c"
-writeRaster(rar, file.path(outputs, "aq_lakes_rarity_c.tif"))
+writeRaster(rar, file.path(outputs, "aq_lakes_rarity_c.tif"), overwrite = T)
 
 # rarity - classed lakes 
 
 rc <- rast(file.path("outputs", "sk_lakes_raritybyarea_conc.tif"))
+rc<- aggregate(rc , fact=10, fun="mean")
 names(rc )= "aq_lakes_rarity_class"
-writeRaster(rc, file.path(outputs, "aq_lakes_rarity_class.tif"))
+writeRaster(rc, file.path(outputs, "aq_lakes_rarity_class.tif"), overwrite = T)
 
 
 # diversity - continuous lakes
 div <- rast(file.path("outputs", "sk_lakes_meanbyarea_div_101c.tif"))
+div<- aggregate(div , fact=10, fun="mean")
 names(div)= "aq_lakes_diversity_c"
-writeRaster(div, file.path(outputs, "aq_lakes_diversity_c.tif"))
+writeRaster(div, file.path(outputs, "aq_lakes_diversity_c.tif"), overwrite = T)
 
 # diversity - classed lakes
 div <- rast(file.path("outputs", "sk_lakes_divarea_conc.tif"))
+div <- aggregate(div , fact=10, fun="mean")
 names(div)= "aq_lakes_diversity_class"
-writeRaster(div, file.path(outputs, "aq_lakes_diversity_class.tif"))
+writeRaster(div, file.path(outputs, "aq_lakes_diversity_class.tif"), overwrite = T)
 
-
-## Aquatic terrestrial barcodes - Rivers
-# diversity - continuous
-div <- rast(file.path("outputs", "sk_rivers_diversity_101c.tif"))
-names(div)= "aq_rivers_diversity_c"
-writeRaster(div, file.path(outputs, "aq_rivers_diversity_c.tif"))
-# diversity - classed
-div <- rast(file.path("outputs", "sk_rivers_diversity_conc.tif"))
-names(div)= "aq_rivers_diversity_class"
-writeRaster(div, file.path(outputs, "aq_rivers_diversity_class.tif"))
-
-# rarity - continuous
-rc <- rast(file.path("outputs", "sk_rivers_rarity_mean_101c.tif"))
-names(rc)= "aq_rivers_rarity_c"
-writeRaster(rc, file.path(outputs, "aq_rivers_rarity_c.tif"), overwrite = T)
-
-# rarity - classed
-
-rc <- rast( file.path("outputs", "sk_rivers_rarity_mean_conc.tif"))
-names(rc)= "aq_rivers_rarity_class"
-writeRaster(rc, file.path(outputs, "aq_rivers_rarity_class.tif"), overwrite = T)
+# 
+# ## Aquatic terrestrial barcodes - Rivers
+# # diversity - continuous
+# div <- rast(file.path("outputs", "sk_rivers_diversity_101c.tif"))
+# names(div)= "aq_rivers_diversity_c"
+# writeRaster(div, file.path(outputs, "aq_rivers_diversity_c.tif"))
+# # diversity - classed
+# div <- rast(file.path("outputs", "sk_rivers_diversity_conc.tif"))
+# names(div)= "aq_rivers_diversity_class"
+# writeRaster(div, file.path(outputs, "aq_rivers_diversity_class.tif"))
+# 
+# # rarity - continuous
+# rc <- rast(file.path("outputs", "sk_rivers_rarity_mean_101c.tif"))
+# names(rc)= "aq_rivers_rarity_c"
+# writeRaster(rc, file.path(outputs, "aq_rivers_rarity_c.tif"), overwrite = T)
+# 
+# # rarity - classed
+# 
+# rc <- rast( file.path("outputs", "sk_rivers_rarity_mean_conc.tif"))
+# names(rc)= "aq_rivers_rarity_class"
+# writeRaster(rc, file.path(outputs, "aq_rivers_rarity_class.tif"), overwrite = T)
 
 
 # wetland density 
 wet <-  rast(file.path("inputs", "sk_wetland_density_1km.tif"))
 names(wet) = "aq_wetland_density"
 writeRaster(wet, file.path(outputs, "aq_wetland_density.tif"))
+
 
 # lake density
 
@@ -233,3 +300,28 @@ writeRaster(lak, file.path(outputs, "aq_lake_density.tif"))
 
 
 
+
+
+
+
+
+## check the number of cells 
+# ecoregion 
+
+ec <- st_read(file.path("outputs", "sk_ecoreg_reduced.gpkg")) |> 
+  mutate(ecocode = seq_along(ECOREGION_NAME))
+
+ecv <- vect(ec)
+
+ecr <- rasterize(ecv, srast, field = "ecocode")
+
+# count number of cells per ecoregion
+
+ecr <- ecr |> 
+  as.data.frame() |> 
+  group_by(ecocode) |> 
+  summarise(n = n())
+
+ecdf <- ec %>% st_drop_geometry(ec)
+
+cellno <- left_join(ecr, ecdf)
